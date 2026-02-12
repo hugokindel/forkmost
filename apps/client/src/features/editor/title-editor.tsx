fb8c679a90119d57684586ede982fefb95fe5bff
Philipinho <16838612+Philipinho@users.noreply.github.com>
2026-02-11 22:47:25 -0800

    fix tiptap version conflicts

--- apps/client/src/features/editor/title-editor.tsx
+++ apps/client/src/features/editor/title-editor.tsx
@@ -185,7 +185,7 @@ export function TitleEditor({
       // guard against Cannot access view['hasFocus'] error
       if (!titleEditor?.isInitialized) return;
       titleEditor?.commands?.focus("end");
-    }, 500);
+    }, 300);
   }, [titleEditor]);
 
   useEffect(() => {
